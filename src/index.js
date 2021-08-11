5134020a5c874f5bf1147fd9421a047df6217a8d
Morenikeji Bello <caeserselassie@gmail.com>
2021-08-11 23:57:50 +0100

    Set up UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import './style.css';
+import './assets/stylesheets/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/modal';
+import './assets/images/sheyi-lagos.jpg'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const mainContent = document.querySelector('#main')
+    mainContent.classList.add('default-bg')
+
+
+});
